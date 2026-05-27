4e611dfffd388526ea8cc0508373ac75fe69310d
HeitorAlvesSenai <0001074589@senaimgaluno.com.br>
2026-05-27 11:09:17 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/compras', function () {
+    return view('compras');
+});
+Route::get('/cadastro', function() {
+    return view('cadastro');
+});
